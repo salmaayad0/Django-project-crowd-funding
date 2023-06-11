920a29a813404f223430ff5e6bf2c83b21370306
eman mohamed <eman.mohamed1832000@gmail.com>
2023-06-11 20:04:49 +0300

    verification and login

--- crowdFund/users/urls.py
+++ crowdFund/users/urls.py
@@ -3,6 +3,10 @@ from django.urls import path
 
 urlpatterns = [
     path('login', views.login, name='login'),
+        path('index', views.index, name='index'),
+
+      path("verification",views.verification,name="verification"),
+
   path("",views.registeration,name="regesteration")
 
 ]
--- crowdFund/users/views.py
+++ crowdFund/users/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from django.http import HttpResponseRedirect  
 
 from .models import Users, Login
 from .forms import LoginForm,RegForm
@@ -7,28 +8,37 @@ from .forms import LoginForm,RegForm
 # Create your views here.
 def login(request):
     if request.method == 'POST':
-     loginData = LoginForm(request.POST)
-     data = Users.objects.all()
-     if loginData.is_valid():
-         email = request.POST.get('email')
-         password = request.POST.get('password')
-         print(email)
-         for user in data:
-             dbEmail = user.email
-             dbpassword = user.password
-             print(dbEmail)
-             if email == dbEmail and password == dbpassword:
-                user = {
-                'email': email,
-                'password': password }
-                return HttpResponse('users/index.html')
-             else:
-                return HttpResponse('users/register.html')
+       loginData = LoginForm(request.POST)
+       if loginData.is_valid():
+            userdata=Users.objects.all()
+            email=request.POST.get('email')
+            password=request.POST.get('password')
+            print(email)
+            emails=[]
+            for user in userdata:
+                print(user.email)
+                print(user.password)
+                emails.append(user.email)
+                print(emails)
+            if email in emails  :
+                    print("email already exist")
+                    if user.password==password :
+                     return HttpResponseRedirect("/index")
+                 
+            else:
+                 return HttpResponseRedirect("/login")
         
     return render(request, 'users/login.html', {'formLogin':LoginForm})
 
 
+def verification(request):
+    
 
+    return render(request, 'users/verification.html')
+def index(request):
+    
+
+    return render(request, 'users/index.html')
 
 def registeration(request):
     if request.method == "POST":
@@ -43,10 +53,13 @@ def registeration(request):
                 emails.append(user.email)
                 print(emails)
             if email in emails  :
-                    print("email already exist") 
+                    print("email already exist")
+                    return HttpResponseRedirect("/login")
+ 
                     
             else:
                     data.save()
+                    return HttpResponseRedirect("/verification")
                     
     
         
