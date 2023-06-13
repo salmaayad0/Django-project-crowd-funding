63b9c3a3f6443479f4cdd548f13242924061d560
salmaayad0 <salmaayad017@gmail.com>
2023-06-13 16:55:26 +0300

    category and templates update

--- crowdFund/projects/urls.py
+++ crowdFund/projects/urls.py
@@ -2,6 +2,10 @@ from . import views
 from django.urls import path
 
 urlpatterns = [
-    path('', views.index, name='index'),
-    path('/add', views.addProject, name='addProject')
+    path('/add', views.addProject, name='addProject'),
+    path('/education', views.edu_category, name='education'),
+    path('/entertainment', views.entertainment_category, name='entertainment'),
+    path('/sports', views.sports_category, name='sports'),
+    path('/fashon', views.fashon_category, name='fashon')
+    
 ]
--- crowdFund/projects/views.py
+++ crowdFund/projects/views.py
@@ -3,21 +3,50 @@ from django.http import HttpResponse, HttpResponseRedirect
 
 from .models import Project
 
-# Create your views here.
-def index(request):
-    return HttpResponse("projects")
-
-
 # add project 
 def addProject(request):
     if(request.method == 'POST'):
-        Project.objects.create(title = request.POST['title'])
-        Project.objects.create(details = request.POST['details'])
-        Project.objects.create(category = request.POST['category'])
-        Project.objects.create(mutliImage = request.POST['mutliImage'])
-        Project.objects.create(totalTarget = request.POST['totalTarget'])
-        Project.objects.create(tag = request.POST['tag'])
-        Project.objects.create(startDate = request.POST['startDate'])
         
         return HttpResponseRedirect('/projects')
     return render(request, 'projects/addProject.html')
+
+
+# projects category 
+# eduaction
+def edu_category(request):
+    selectedProj = []
+    projects = Project.objects.all()
+    for project in projects:
+        if project.category == 'education':
+            selectedProj.append(project)
+    return render(request, 'projects/education.html', {'projects': selectedProj})
+
+# entertainment
+def entertainment_category(request):
+    selectedProj = []
+    projects = Project.objects.all()
+    for project in projects:
+        if project.category == 'entertainment':
+            selectedProj.append(project)
+    return render(request, 'projects/entertainment.html', {'projects': selectedProj})
+
+
+# sports
+def sports_category(request):
+    selectedProj = []
+    projects = Project.objects.all()
+    for project in projects:
+        if project.category == 'sports':
+            selectedProj.append(project)
+    return render(request, 'projects/sports.html', {'projects': selectedProj})
+
+
+# fashon
+def fashon_category(request):
+    selectedProj = []
+    projects = Project.objects.all()
+    for project in projects:
+        if project.category == 'fashon':
+            selectedProj.append(project)
+    return render(request, 'projects/fashon.html', {'projects': selectedProj})
+
